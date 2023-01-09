736a55a09b4489e145f6868fbcaa25c02410dfb9
Carl <luohuazju@gmail.com>
2023-01-08 20:26:40 -0600

    generate the file name

--- inappropriate-video-detect/lambda-apigateway-s3/src/main/java/com/aws/lambda/handler/FileUploadHandler.java
+++ inappropriate-video-detect/lambda-apigateway-s3/src/main/java/com/aws/lambda/handler/FileUploadHandler.java
@@ -3,6 +3,7 @@ package com.aws.lambda.handler;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.math.BigInteger;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,29 +20,36 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class FileUploadHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
 	private Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
+	private String generateNameOnContent(String originalName) throws NoSuchAlgorithmException {
+		MessageDigest m = MessageDigest.getInstance("MD5");
+		m.update(originalName.getBytes(), 0, originalName.length());
+		return new BigInteger(1, m.digest()).toString(16);
+	}
+
 	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
 
 		LambdaLogger logger = context.getLogger();
-		//logger.log("event:" + event);
-	
-		//logger.log("event json: " + gson.toJson(event));
+		// logger.log("event:" + event);
+
+		// logger.log("event json: " + gson.toJson(event));
 
 		logger.log("1 Incoming body size: " + String.valueOf(event.getBody().getBytes().length));
 		String clientRegion = "us-east-1";
 		String bucketName = "rekognition-videos-bucket";
 		String fileObjKeyName = "video_sample.mp4";
-		
+		String fileObjHashName = "default.mp4";
 
 		APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
 
 		try {
 			// Get the uploaded file and decode from base64
-			
 			byte[] bI = Base64.getDecoder().decode(event.getBody().getBytes());
 			logger.log("2 Decode the base64 from body");
 
@@ -51,7 +59,7 @@ public class FileUploadHandler implements RequestHandler<APIGatewayV2HTTPEvent, 
 			if (hps != null) {
 				logger.log("2.1 get content type from map");
 				contentType = hps.get("Content-Type");
-				
+
 			}
 			logger.log("3 get content type:" + contentType);
 
@@ -63,7 +71,7 @@ public class FileUploadHandler implements RequestHandler<APIGatewayV2HTTPEvent, 
 
 			// Log the extraction for verification purposes
 			// logger.log("3.1 " + new String(bI, "UTF-8") + "\n");
-			
+
 			logger.log("4 get boundary done");
 
 			// Create a ByteArrayInputStream
@@ -77,7 +85,7 @@ public class FileUploadHandler implements RequestHandler<APIGatewayV2HTTPEvent, 
 
 			// Find first boundary in the MultipartStream
 			boolean nextPart = multipartStream.skipPreamble();
-			
+
 			logger.log("5 prepare the data");
 
 			// Loop through each segment
@@ -90,8 +98,11 @@ public class FileUploadHandler implements RequestHandler<APIGatewayV2HTTPEvent, 
 					int index = header.indexOf("filename=");
 					int secondIndex = header.indexOf(".mp4");
 					if (index > 0 && secondIndex > 0) {
-						fileObjKeyName = header.substring(index + 10, secondIndex) + ".mp4";
+						fileObjKeyName = header.substring(index + 10, secondIndex);
 						logger.log("5.2 fileObjKeyName:" + fileObjKeyName);
+
+						fileObjHashName = generateNameOnContent(fileObjKeyName) + ".mp4";
+						logger.log("5.3 fileObjHashName:" + fileObjHashName);
 					}
 				}
 
@@ -120,7 +131,7 @@ public class FileUploadHandler implements RequestHandler<APIGatewayV2HTTPEvent, 
 			metadata.setCacheControl("public, max-age=31536000");
 
 			// Put file into S3
-			s3Client.putObject(bucketName, fileObjKeyName, fis, metadata);
+			s3Client.putObject(bucketName, fileObjHashName, fis, metadata);
 
 			// Log status
 			logger.log("8 Put object in S3");
@@ -132,12 +143,11 @@ public class FileUploadHandler implements RequestHandler<APIGatewayV2HTTPEvent, 
 			headers.put("Content-Type", "application/json");
 			response.setHeaders(headers);
 			response.setBody(gson.toJson("OK"));
-			
-			
+
 		} catch (Exception e) {
 			logger.log("Exception: " + e.getMessage());
 		}
-		
+
 		return response;
 
 	}

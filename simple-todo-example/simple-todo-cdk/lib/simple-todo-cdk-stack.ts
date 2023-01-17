6a077162e849d33893a01ab90a242ab509153ef7
Carl <luohuazju@gmail.com>
2023-01-16 22:11:53 -0600

    working example

--- simple-todo-example/simple-todo-cdk/lib/simple-todo-cdk-stack.ts
+++ simple-todo-example/simple-todo-cdk/lib/simple-todo-cdk-stack.ts
@@ -1,16 +1,53 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import lambda = require('@aws-cdk/aws-lambda-nodejs');
+import sqs = require('@aws-cdk/aws-sqs');
+import dynamodb = require('@aws-cdk/aws-dynamodb');
+import { ApiEventSource, SqsEventSource } from '@aws-cdk/aws-lambda-event-sources';
+import { Runtime } from '@aws-cdk/aws-lambda';
 
 export class SimpleTodoCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // define our DynamoDB table
+    const dynamoTable = new dynamodb.Table(this, 'cdk-todonts-table', {
+      tableName: 'cdk-todonts-table',
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SimpleTodoCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // define our SQS buffer queue
+    const sqsBuffer = new sqs.Queue(this, 'cdk-todonts-queue', {
+      queueName: 'cdk-todonts-queue',
+    });
+
+
+    // define our processing lambda
+    const processLambda = new lambda.NodejsFunction(this, 'cdk-todonts-process', {
+      runtime: Runtime.NODEJS_14_X,
+      handler: 'handler',
+      entry: 'src/process-handler.js',
+      events: [new SqsEventSource(sqsBuffer)],
+      environment: {
+        TABLE_NAME: dynamoTable.tableName
+      }
+    });
+
+    // grant write access for the processing lambda to our dynamo table
+    dynamoTable.grantWriteData(processLambda);
+
+    // define the lambda post API
+    const postLambda = new lambda.NodejsFunction(this, 'cdk-todonts-post', {
+      runtime: Runtime.NODEJS_14_X,
+      entry: 'src/post-handler.js',
+      handler: 'handler',
+      events: [new ApiEventSource('POST', '/todonts')],
+      environment: {
+        QUEUE_URL: sqsBuffer.queueUrl,
+      }
+    });
+
+    // grant write access to the SQS buffer queue for our API lambda
+    sqsBuffer.grantSendMessages(postLambda);
+
   }
 }
